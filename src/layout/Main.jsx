70a18d4fcc4a081e212e86a6458c5bd57f61f402
Marat Shakirov <unclemarat@gmail.com>
2021-06-08 15:53:02 +0300

    second commit

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,20 +3,23 @@ import Movies from '../component/Movies';
 import Preloader from '../component/Preloader';
 import Search from '../component/Search';
 
+const API_KEY = process.local.REACT_APP_API_KEY;
+
 export default class Main extends Component {
   state = {
     movies: [],
     loading: true
   };
+  
   componentDidMount() {
-    fetch('http://www.omdbapi.com/?apikey=f296da7b&s=avengers')
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=avengers`)
       .then((response) => response.json())
       .then((data) => this.setState({ movies: data.Search, loading: false }));
   }
   searchFilm = (str, type = 'all') => {
     this.setState({loading: true})
     fetch(
-      `http://www.omdbapi.com/?apikey=f296da7b&s=${str}${type !== 'all' ? `&type=${type}` : ''}`
+      `http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`
       )
       .then((response) => response.json())
       .then((data) => this.setState({ movies: data.Search, loading: false }));
